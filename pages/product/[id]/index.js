868ec7acf4a48ce20cbea7ec33bbf5d19e9d7d0e
nomelancholy <starmekey@naver.com>
2021-07-21 22:44:06 +0900

    [U] 페이지에서 token 찾아서 컴포넌트로 전달

--- pages/product/[id]/index.js
+++ pages/product/[id]/index.js
@@ -2,16 +2,24 @@ import React from "react";
 import { connect } from "react-redux";
 import { useRouter } from "next/router";
 import ProductDetail from "@components/product/productDetail";
+import { wrapper } from "@state/stores";
+import initialize from "@utils/initialize";
 
-const ProductWithId = () => {
+const ProductWithId = (props) => {
   const router = useRouter();
+
   const { id } = router.query;
+  const { user, isLoggedIn, token } = props.auth;
 
   return (
     <>
-      <ProductDetail productId={id} />
+      <ProductDetail productId={id} token={token} />
     </>
   );
 };
+
+export const getServerSideProps = wrapper.getServerSideProps((ctx) => {
+  return { props: initialize(ctx) };
+});
 
 export default connect((state) => state)(ProductWithId);
