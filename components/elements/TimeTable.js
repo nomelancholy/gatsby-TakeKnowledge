58097f470181c9c3fb3fccb3f653fa529570ccad
nomelancholy <starmekey@naver.com>
2021-08-19 10:09:05 +0900

    [U] TimeTable 컴포넌트 추가

--- components/elements/TimeTable.js
+++ components/elements/TimeTable.js
@@ -1,32 +1,53 @@
-import { Tag } from "antd";
-import React, { useState, useEffect } from "react";
+import React, { useState, useEffect, useRef } from "react";
+
+import FullCalendar from "@fullcalendar/react";
+import interactionPlugin from "@fullcalendar/interaction";
+import timegridPlugin from "@fullcalendar/timegrid";
+// import timegridPlugin from "@fullcalendar/resource-timegrid";
+import timelinePlugin from "@fullcalendar/resource-timeline";
+import moment, { locale } from "moment";
+import { format, addMinutes } from "date-fns";
+import { FullCalendarLicense } from "@utils/config";
 
 const TimeTable = (props) => {
-  const { id, label, onChange, checked } = props;
-  const [isChecked, setIsChecked] = useState(false);
-
-  useEffect(() => {
-    if (checked) {
-      setIsChecked(checked);
-    } else {
-      setIsChecked(false);
-    }
-  }, []);
-
-  const handleChange = (checked) => {
-    setIsChecked(checked);
-    onChange(id, checked);
-  };
+  const calendarRef = useRef(null);
 
   return (
-    <ul
-      onDragOver={(e) => {
-        e.preventDefault();
-        console.log(`e`, e);
+    <FullCalendar
+      ref={calendarRef}
+      schedulerLicenseKey={FullCalendarLicense}
+      plugins={[interactionPlugin, timegridPlugin]}
+      initialView="timeGridWeek"
+      height="auto"
+      handleWindowResize={true}
+      headerToolbar={{
+        left: "",
+        center: "",
+        right: "",
       }}
-    >
-      <li draggable>테스트</li>
-    </ul>
+      dayHeaderContent={(date) => {
+        let weekList = ["일", "월", "화", "수", "목", "금", "토"];
+        return weekList[date.dow];
+      }}
+      allDaySlot={false}
+      slotDuration={{ hours: 1 }}
+      slotMinTime="00:00:00"
+      slotMaxTime="24:00:00"
+      slotMinWidth={60}
+      slotLabelContent={(arg) => {
+        const timeHour = format(arg.date, "H시");
+        return timeHour;
+      }}
+      selectable={true}
+      select={(info, start, end, jsEvent, view) => {
+        console.log(`select info`, info);
+        console.log(`calendarRef`, calendarRef);
+      }}
+      selectOverlap={(event) => {
+        console.log(`event`, event);
+      }}
+      unselectAuto={false}
+    />
   );
 };
 
